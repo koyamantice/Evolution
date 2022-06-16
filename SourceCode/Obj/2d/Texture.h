040b1b81cdc0d694d06061b6827c762877dda314
koyamakaito <k020g1197@g.neec.ac.jp>
2022-06-17 07:30:46 +0900

    ギットから除外されてた奴ら帰還

---
+++ SourceCode/Obj/2d/Texture.h
@@ -0,0 +1,194 @@
+#pragma once
+
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <DirectXMath.h>
+#include <d3dx12.h>
+#include "Camera.h"
+
+class Texture
+{
+private: // エイリアス
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+
+public: // サブクラス
+	// 頂点データ構造体
+	struct VertexPosNormalUv
+	{
+		XMFLOAT3 pos; // xyz座標
+		XMFLOAT3 normal; // 法線ベクトル
+		XMFLOAT2 uv;  // uv座標
+	};
+
+	// 定数バッファ用データ構造体
+	struct ConstBufferData
+	{
+		XMFLOAT4 color;	// 色 (RGBA)
+		XMMATRIX mat;	// ３Ｄ変換行列
+	};
+
+private: // 定数
+	static const int division = 50;					// 分割数
+	static const float radius;				// 底面の半径
+	static const float prizmHeight;			// 柱の高さ
+	static const int planeCount = division * 2 + division * 2;		// 面の数
+	static const int vertexCount = 4;		// 頂点数
+	static const int indexCount = 3 * 2;
+public: // 静的メンバ関数
+	/// 静的初期化
+	static bool StaticInitialize(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, int window_width, int window_height, Camera* camera=nullptr);
+
+	/// テクスチャ読み込み
+	static bool LoadTexture(UINT texnumber, const wchar_t* filename);
+
+	/// 描画前処理
+	static void PreDraw();
+
+	/// 描画後処理
+	static void PostDraw();
+	//テクスチャ生成
+	static Texture* Create(UINT texNumber, XMFLOAT3 position, XMFLOAT3 size, XMFLOAT4 color);
+	/// 視点座標の取得
+	static const XMFLOAT3& GetEye() { return eye; }
+
+	/// 視点座標の設定
+	static void SetEye(XMFLOAT3 eye);
+	/// <summary>
+/// カメラセット
+/// </summary>
+/// <returns></returns>
+	static void SetCamera(Camera* camera) {
+		Texture::camera = camera;
+	}
+
+	/// 注視点座標の取得
+	static const XMFLOAT3& GetTarget() { return target; }
+
+	/// 注視点座標の設定
+	static void SetTarget(XMFLOAT3 target);
+
+	/// ベクトルによる移動
+	static void CameraMoveVector(XMFLOAT3 move);
+
+	/// スプライト生成
+	static Texture* Create(UINT texNumber, XMFLOAT2 position, XMFLOAT4 color = { 1, 1, 1, 1 }, XMFLOAT2 anchorpoint = { 0.0f, 0.0f }, bool isFlipX = false, bool isFlipY = false);
+	
+	
+private: // 静的メンバ変数
+	static const int srvCount = 213;
+	// デバイス
+	static ID3D12Device* device;
+	// デスクリプタサイズ
+	static UINT descriptorHandleIncrementSize;
+	// コマンドリスト
+	static ID3D12GraphicsCommandList* cmdList;
+	// ルートシグネチャ
+	static ComPtr<ID3D12RootSignature> rootsignature;
+	// パイプラインステートオブジェクト
+	static ComPtr<ID3D12PipelineState> pipelinestate;
+	// デスクリプタヒープ
+	static ComPtr<ID3D12DescriptorHeap> descHeap;
+	// 頂点バッファ
+	static ComPtr<ID3D12Resource> vertBuff;
+	// インデックスバッファ
+	static ComPtr<ID3D12Resource> indexBuff;
+	// テクスチャバッファ
+	static ComPtr<ID3D12Resource> texbuff[srvCount];
+	// シェーダリソースビューのハンドル(CPU)
+	static CD3DX12_CPU_DESCRIPTOR_HANDLE cpuDescHandleSRV;
+	// シェーダリソースビューのハンドル(CPU)
+	static CD3DX12_GPU_DESCRIPTOR_HANDLE gpuDescHandleSRV;
+	// ビュー行列
+	static XMMATRIX matView;
+	// 射影行列
+	static XMMATRIX matProjection;
+	// 視点座標
+	static XMFLOAT3 eye;
+	// 注視点座標
+	static XMFLOAT3 target;
+	// 上方向ベクトル
+	static XMFLOAT3 up;
+	// ビルボード行列
+	static XMMATRIX matBillboard;
+	// Y軸回りビルボード行列
+	static XMMATRIX matBillboardY;
+	// カメラ
+	static Camera* camera;
+	// 頂点バッファビュー
+	static D3D12_VERTEX_BUFFER_VIEW vbView;
+	// インデックスバッファビュー
+	static D3D12_INDEX_BUFFER_VIEW ibView;
+	// 頂点データ配列
+	static VertexPosNormalUv vertices[vertexCount];
+	// 頂点インデックス配列
+	static unsigned short indices[indexCount];
+	private:
+		UINT texNumber = 0;
+
+private:// 静的メンバ関数
+	/// デスクリプタヒープの初期化
+	static bool InitializeDescriptorHeap();
+
+	/// カメラ初期化
+	static void InitializeCamera(int window_width, int window_height);
+
+	/// グラフィックパイプライン生成
+	static bool InitializeGraphicsPipeline();
+
+	/// モデル作成
+	static void CreateModel();
+
+	/// ビュー行列を更新
+	static void UpdateViewMatrix();
+
+public: // メンバ関数
+	void TextureCreate();
+	//コンストラクタ
+	Texture(UINT texNumber, XMFLOAT3 position, XMFLOAT3 size, XMFLOAT4 color);
+	bool Initialize();
+	/// 毎フレーム処理
+	void Update();
+
+	/// 描画
+	void Draw();
+
+	/// 座標の取得
+	const XMFLOAT3& GetPosition() { return position; }
+
+	/// 座標の設定
+	void SetPosition(XMFLOAT3 position) { this->position = position; }
+	void SetPosition(float x,float y,float z) 
+	{ this->position.x =x; 
+	this->position.y = y;
+	this->position.z = z;
+	}
+	void SetIsBillboard(const bool& isBillboard);
+	void Texture::SetColor(XMFLOAT4 color);
+	void SetRotation(XMFLOAT3 rotation) { this->rotation = rotation; }
+	void SetScale(XMFLOAT3 scale) { this->scale = scale; }
+	const XMFLOAT3& GetScale() { return scale; }
+private: // メンバ変数
+	ComPtr<ID3D12Resource> constBuff; // 定数バッファ
+	// 色
+	XMFLOAT4 color = { 1,1,1,1 };
+	// ローカルスケール
+	XMFLOAT3 scale = { 1,1,1 };
+	// X,Y,Z軸回りのローカル回転角
+	XMFLOAT3 rotation = { 0,0,0 };
+	// ローカル座標
+	XMFLOAT3 position = { 0,0,0 };
+	// ローカルワールド変換行列
+	XMMATRIX matWorld;
+	// ビルボード
+	bool isBillboard = false;
+
+};
+
+
