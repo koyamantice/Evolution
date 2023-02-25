bf1e452a564f3000ac649f96251c3c97c645d21f
koyamakaito <k020g1197@g.neec.ac.jp>
2023-02-25 23:21:05 +0900

    整理

---
+++ SourceCode/User/Actor/Boss/CowKing.h
@@ -0,0 +1,28 @@
+#pragma once
+#include"Boss.h"
+
+class Bullet;
+
+class MashGhost :public Boss {
+public:
+	MashGhost(){};
+
+protected:
+	void OnInit()override;
+	void OnUpda()override;
+	void OnFirstDraw(DirectXCommon* dxCommon)override;
+	void OnDraw(DirectXCommon* dxCommon)override;
+	void OnLastDraw(DirectXCommon* dxCommon)override;
+	void OnFinal()override;
+
+	//オーバーライドメンバ関数
+	void StartAction() override;
+	void AttackPredict() override;
+	void PressAttack() override;
+
+
+	std::unique_ptr<EnemyAttack> attack_;
+
+	float speed = 0.2f;
+	const float accel = speed / 30.0f;
+};
